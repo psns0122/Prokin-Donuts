9b1333baf05cc0d14606c0eb3eee31e07264bcf7
Cmk1031 <maple1031@naver.com>
2025-03-15 20:57:24 +0900

    feat: 입고 검수 기능 승인상태 반환, 완료상태 수정 기능 정의

--- src/repository/InboundRepo.java
+++ src/repository/InboundRepo.java
@@ -1,4 +1,29 @@
 package repository;
 
+import dto.InboundDTO;
+
+import java.util.List;
+
 public interface InboundRepo {
+
+    /**
+     * [입고 검수 기능]
+     * 입고테이블에서 입고승인 상태인 행을 가져온다.
+     * null 인 경우도 생각
+     */
+    List<InboundDTO> findByApprovedStatus();
+
+    /**
+     * [입고 검수 기능]
+     * 입고테이블에서 해당 ID의 입고 상태를 입고완료 상태로 변경한다.
+     * + 트리거를 통해 재고에 반영(update)
+     */
+    void updateCompletedStatus(int inboundId);
+
+    /**
+     *
+     */
+
+
+
 }
--- src/repository/InboundRepoImpl.java
+++ src/repository/InboundRepoImpl.java
@@ -1,4 +1,17 @@
 package repository;
 
+import dto.InboundDTO;
+
+import java.util.List;
+
 public class InboundRepoImpl implements InboundRepo {
+    @Override
+    public List<InboundDTO> findByApprovedStatus() {
+        return null;
+    }
+
+    @Override
+    public void updateCompletedStatus(int inboundId) {
+
+    }
 }
