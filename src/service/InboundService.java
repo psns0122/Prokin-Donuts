2b9f5bdc5760443df357f7d69254a70aaecc702e
Cmk1031 <maple1031@naver.com>
2025-03-15 22:52:45 +0900

    feat/입고 검수 기능(service)

--- src/service/InboundService.java
+++ src/service/InboundService.java
@@ -1,4 +1,26 @@
 package service;
 
+import dto.InboundDTO;
+
+import java.util.List;
+
 public interface InboundService {
+
+    //[입고 검수 Service]
+
+    /**
+     * 입고(승인) 고지서를 반환하는 service
+     * findByApprovedStatus 사용
+     *
+     * @return 입고승인 리스트
+     */
+    List<InboundDTO> getInboundList();
+
+    /**
+     * 입고(승인) ok 되면 입고 완료! service
+     * @param inboundId 입고 아이디
+     */
+    void completedInbound(int inboundId);
+
+
 }
--- src/service/InboundServiceImpl.java
+++ src/service/InboundServiceImpl.java
@@ -1,8 +1,25 @@
 package service;
 
+import dto.InboundDTO;
 import repository.InboundRepo;
 
+import java.util.List;
+
 public class InboundServiceImpl implements InboundService {
+    private final InboundRepo inboundRepo;
+
     public InboundServiceImpl(InboundRepo inboundRepo) {
+        this.inboundRepo = inboundRepo;
+    }
+
+
+    @Override
+    public List<InboundDTO> getInboundList() {
+        return null;
+    }
+
+    @Override
+    public void completedInbound(int inboundId) {
+
     }
 }
