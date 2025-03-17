3f24d41ee42c29461e442265bee03c35d7043bb2
kya <dbsdk4211@naver.com>
2025-03-17 17:28:57 +0900

    feat : repo의 [회원 추가 기능], [회원 수정 기능 ] 추가
    해당 기능의 프로시저를 호출하여 기능 구현
    
    #62

--- src/repository/MemberRepoImpl.java
+++ src/repository/MemberRepoImpl.java
@@ -1,44 +1,108 @@
 package repository;
 
-import dto.MemberDTO;
+import config.DBUtil;
+import dto.memberDTO.MemberDTO;
+import vo.memberVO.MemberVO;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class MemberRepoImpl implements MemberRepo {
 
-    @Override
-    public MemberDTO insertMember(MemberDTO member) {
-        return null;
-    }
 
+    Connection conn = null;
+    CallableStatement cs = null;
+
+    /* 회원 등록 메서드
+    * 총관리자는 창고관리자를 등록한다.
+    * */
     @Override
-    public Optional<MemberDTO> updateMember(String memberId, MemberDTO updateMember) {
+    public Optional<MemberVO> insertMember(MemberVO member) {
+        conn = DBUtil.getConnection();
+
+        try {
+            String sql = "{call insertMember('member',?,?,?,?,?,?,?,?)}";
+
+            cs = conn.prepareCall(sql);
+
+            cs.setInt(1, member.getMemberNo());
+            cs.setInt(2, member.getAuthorityId());
+            cs.setString(3,member.getName());
+            cs.setString(4,member.getPhoneNumber());
+            cs.setString(5,member.getEmail());
+            cs.setString(6,member.getAddress());
+            cs.setString(7,member.getId());
+            cs.setString(8,member.getPassword());
+
+            int rs = cs.executeUpdate();
+
+            //실행 성공 시 객체 반환, 실패 시 빈 optional반환
+            if(rs > 0) return  Optional.of(member);
+            else return Optional.empty();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtil.closeQuietly(null,cs,conn);
+        }
         return Optional.empty();
     }
 
     @Override
-    public Optional<MemberDTO> deleteMember(String memberId) {
+    public Optional<MemberVO> updateMember(String memberId, MemberVO updateMember) {
+        conn =DBUtil.getConnection();
+
+        try {
+            String sql = "{call updateMember(?, ?, ? ,? ,? ,? ,? )}";
+            cs = conn.prepareCall(sql);
+
+            cs.setString(1,memberId);
+            cs.setString(2,updateMember.getName());
+            cs.setString(3,updateMember.getPhoneNumber());
+            cs.setString(4,updateMember.getEmail());
+            cs.setString(5,updateMember.getAddress());
+            cs.setString(6,updateMember.getId());
+            cs.setString(7,updateMember.getPassword());
+
+            int rs = cs.executeUpdate();
+            //실행 성공 시 객체 반환, 실패 시 빈 optional반환
+            if(rs > 0) return  Optional.of(updateMember);
+            else return Optional.empty();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtil.closeQuietly(null,cs,conn);
+            }
         return Optional.empty();
     }
 
     @Override
-    public boolean requestMember(MemberDTO member) {
+    public Optional<String> deleteMember(String memberId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean requestMember(MemberVO member) {
         return false;
     }
 
     @Override
-    public Optional<MemberDTO> approvalMember(String memberId) {
+    public Optional<MemberVO> approvalMember(String memberId) {
         return Optional.empty();
     }
 
     @Override
-    public <T> Optional<List<MemberDTO>> loadMember(String searchAttribute, T serchValue) {
+    public <T> Optional<List<MemberVO>> loadMember(String searchAttribute, T serchValue) {
         return Optional.empty();
     }
 
     @Override
-    public Optional<List<MemberDTO>> allLoadMember() {
+    public Optional<List<MemberVO>> allLoadMember() {
         return Optional.empty();
     }
 
@@ -53,7 +117,7 @@ public class MemberRepoImpl implements MemberRepo {
     }
 
     @Override
-    public Optional<List<MemberDTO>> loadRequestMember() {
+    public Optional<List<MemberVO>> loadRequestMember() {
         return Optional.empty();
     }
 }
