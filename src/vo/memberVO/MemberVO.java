b956f725e54eaf22861ff71cff755539ed9c7484
kya <dbsdk4211@naver.com>
2025-03-17 15:10:37 +0900

    feat : memberreauestVO 추가 및 membervo package 분리

--- src/vo/memberVO/MemberVO.java
+++ src/vo/memberVO/MemberVO.java
@@ -1,4 +1,4 @@
-package vo;
+package vo.memberVO;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
---
+++ src/vo/memberVO/memberreauestVO.java
@@ -0,0 +1,20 @@
+package vo.memberVO;
+
+public class memberreauestVO {
+    /** 권한 아이디 */
+    private String authorityId;
+    /** 회원 이름 */
+    private String name;
+    /** 회원 전화번호 */
+    private String phoneNumber;
+    /** 회원 이메일 */
+    private String email;
+    /** 회원 주소 */
+    private String address;
+    /** 회원 아이디 */
+    private String id;
+    /** 회원 비밀번호 */
+    private String password;
+    /** 회원 승인요청 상태 **/
+    private String request;
+}
