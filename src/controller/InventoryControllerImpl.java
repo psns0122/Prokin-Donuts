17777243de6b6de5752e5332d37aeaf7e05eb396
psns <psns0122@naver.com>
2025-03-17 16:49:19 +0900

    chore: DiConfig 생성자 오류 해결

--- src/controller/InventoryControllerImpl.java
+++ src/controller/InventoryControllerImpl.java
@@ -5,6 +5,9 @@ import service.InventoryService;
 import java.util.Scanner;
 
 public class InventoryControllerImpl implements InventoryController {
+    public InventoryControllerImpl(InventoryService inventoryService) {
+
+    }
 
 
 }
