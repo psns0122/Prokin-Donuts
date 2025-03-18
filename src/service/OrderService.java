67a59b1d14d9c241c96026decd3fbbaaae390843
jyngmyungchae <jyngmyungchae@gmail.com>
2025-03-18 19:24:37 +0900

    feat: 발주요청 비즈니스로직 구현

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -1,4 +1,15 @@
 package service;
 
+import dto.OrderDTO;
+import vo.OrderVO;
+import vo.OrderDetailVO;
+import vo.OrderStatisticsDTO;
+import java.util.List;
+import java.util.Map;
+
 public interface OrderService {
+    String submitOrder(OrderDTO dto);
+    OrderVO getOrder(String orderId);
+    List<OrderDetailVO> getOrderDetails(String orderId);
+    OrderStatisticsDTO getOrderStatisticsByFranchiseAndMonth(String franchiseId, int year, int month);
 }
