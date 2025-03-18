4118dfbb82adcfd2c632e3fe1e48dc163106e1da
jyngmyungchae <jyngmyungchae@gmail.com>
2025-03-18 19:13:44 +0900

    feat: 주문 상세 항목을 담는 객체

---
+++ src/vo/orderVO/OrderDetailVO.java
@@ -0,0 +1,20 @@
+package vo;
+
+public class OrderDetailVO {
+    private int orderDetailId;
+    private int orderQuantity;
+    private String productId;
+    private String orderId;
+
+    public OrderDetailVO(int orderDetailId, int orderQuantity, String productId, String orderId) {
+        this.orderDetailId = orderDetailId;
+        this.orderQuantity = orderQuantity;
+        this.productId = productId;
+        this.orderId = orderId;
+    }
+
+    public int getOrderDetailId() { return orderDetailId; }
+    public int getOrderQuantity() { return orderQuantity; }
+    public String getProductId() { return productId; }
+    public String getOrderId() { return orderId; }
+}
