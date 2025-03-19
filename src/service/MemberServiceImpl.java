4193bb2d4eaad8025be507e5c438f720bde4b689
kya <dbsdk4211@naver.com>
2025-03-19 12:27:00 +0900

    feat : 아이디 중복검사 기능 추가

--- src/service/MemberServiceImpl.java
+++ src/service/MemberServiceImpl.java
@@ -17,10 +17,13 @@ public class MemberServiceImpl implements MemberService {
     //회원 등록 기능
     @Override
     public MemberDTO addMember(MemberDTO member) {
-        //insertMember 반환된 객체를 result 에 저장
-        Optional<MemberDTO> result = memberRepo.insertMember(member);
-        //결과값을 반환, 하지만 결과값이 optional.empty면 null 반환
-        return result.orElse(null);
+        if (checkId(member.getId())) return null; // 중복검사
+        else {
+            //insertMember 반환된 객체를 result 에 저장
+            Optional<MemberDTO> result = memberRepo.insertMember(member);
+            //결과값을 반환, 하지만 결과값이 optional.empty면 null 반환
+            return result.orElse(null);
+        }
     }
 
     //회원 수정 기능
@@ -43,8 +46,11 @@ public class MemberServiceImpl implements MemberService {
     //회원 가입 기능
     @Override
     public MemberRequestDTO requestMember(MemberRequestDTO member) {
-        Optional<MemberRequestDTO> result = memberRepo.requestMember(member);
-        return result.orElse(null);
+        if (checkId(member.getId())) return null; // 중복검사
+        else {
+            Optional<MemberRequestDTO> result = memberRepo.requestMember(member);
+            return result.orElse(null);
+        }
     }
 
     //회원아이디 중복검사 기능
