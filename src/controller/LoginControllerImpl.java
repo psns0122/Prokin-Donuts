db542539f8cf77eb43caa8f7c25593e71b8b8d6a
kya <dbsdk4211@naver.com>
2025-03-19 16:09:27 +0900

    fix : 로그인 정보에서 권한 아이디 받아오도록 수정

--- src/controller/LoginControllerImpl.java
+++ src/controller/LoginControllerImpl.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 public class LoginControllerImpl implements LoginController{
     MemberService memberService;
+
     public LoginControllerImpl(MemberService memberService) {
         this.memberService = memberService;
     }
